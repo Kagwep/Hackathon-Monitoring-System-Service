e5f4a635a8dbed381c2ee74e49f16ef1bca3db5f
kagwe <kagwepeter07@gmail.com>
2022-02-05 14:20:42 +0300

    'updated'

--- KenyaEMR.py
+++ KenyaEMR.py
@@ -6,9 +6,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-HOSTNAME = ['www.mwandishi.co.ke','www.nhif.co.ke','www.maseno.ac.ke','www.google.com','www.github.com',]
-ports = [8080,80,70,100,200,300,400,8041]
-
+HOSTNAME = 'www.mwandihi.co.ke'
+hostname = HOSTNAME
 services = [
     {"port": "80",
      "description":"Notifiy service"
@@ -34,44 +33,48 @@ services = [
 
 res = [sub['port']for sub in services]
 res1 = [sub['description']for sub in services]
+
+def mail(message):
+    mail_content = message
+    #The mail addresses and password
+    sender_address = 'pkagwe07@gmail.com'
+    sender_pass = 'kagwepeter07@gmail.com'
+    receiver_address = 'kagwepeter07@gmail.com'
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Alert!'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+    
   
 def func():
 
     
     
-    hostname = HOSTNAME
-    for host in hostname:
-        host = str(host)
-        print(host)
-        response = os.system("ping " + host)
+    
+
+    response = os.system("ping " + hostname)
         # and then check the response...
-        if response == 0:
+    if response == 0:
             
-            pingstatus = "Network Active"
-            print(pingstatus)
-        else:
-            pingstatus = "Network Error"
-            print(pingstatus)
-            mail_content = host + ' Server is Down'
-            #The mail addresses and password
-            sender_address = 'pkagwe07@gmail.com'
-            sender_pass = 'kagwepeter07@gmail.com'
-            receiver_address = 'kagwepeter07@gmail.com'
-            #Setup the MIME
-            message = MIMEMultipart()
-            message['From'] = sender_address
-            message['To'] = receiver_address
-            message['Subject'] = 'Alert!'   #The subject line
-            #The body and the attachments for the mail
-            message.attach(MIMEText(mail_content, 'plain'))
-            #Create SMTP session for sending the mail
-            session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-            session.starttls() #enable security
-            session.login(sender_address, sender_pass) #login with mail_id and password
-            text = message.as_string()
-            session.sendmail(sender_address, receiver_address, text)
-            session.quit()
-            print('Mail Sent')
+        pingstatus = "Network Active"
+        print(pingstatus)
+            
+    else:
+        pingstatus = "Network Error"
+        print(pingstatus)
+        mail( hostname + ' server is down')
+
 
     for element in res:
         new_element = int(element)
@@ -83,12 +86,12 @@ def func():
             print(des + ' port is working')
             
         else:
-
-            print(des + ' not working')
+            mail(des + ' not working' + ' in' + hostname )
+            print(des + ' not working' + ' in' + hostname)
   
-schedule.every(5).minutes.do(func)
+schedule.every(0.05).minutes.do(func)
   
 while True:
     schedule.run_pending()
-    time.sleep(5)
+    time.sleep(0.05)
 
