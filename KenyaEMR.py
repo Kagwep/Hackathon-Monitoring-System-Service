280675898cd6aeef1656f9fbc79207e8495f0b0e
Emmanuel Jan <emmanueljan@pop-os.localdomain>
2022-02-08 08:10:44 +0300

    combining the service into a single function and debuging

--- KenyaEMR.py
+++ KenyaEMR.py
@@ -6,127 +6,130 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# in order to use the service, we need adminEmail, hostname, services(array of services)
-adminEmail= 'stephenwanjala145@gmail.com'
-hostname = 'www.mwandishi.co.ke'
+
+# in order to use the service, we need adminEmail, hostname, services(array of services) and time interval
+hostname = 'mwandishi.co.ke'
+adminEmail = 'emmanueljan80@gmail.com'
+timeInterval = 0.05
 services_open = [
     {"port": "8011",
-     "description":"Notifiy service"
-     },
+    "description":"Notifiy service"
+    },
     {"port": "8080",
-     "description":"Database service"
-     },
+    "description":"Database service"
+    },
     {"port": "8041",
-     "description":"Messaging service"
-     },
+    "description":"Messaging service"
+    },
     {"port": "8082",
-     "description":"Triage service"
-     },
+    "description":"Triage service"
+    },
     {"port": "8083",
-     "description":"Reports service"
-     },
+    "description":"Reports service"
+    },
     {"port": "8084",
-     "description":"Lab service"
-     },
+    "description":"Lab service"
+    },
 ]
 
 services_closed = [
     {"port": "80",
-     "description":"Notifiy service"
-     },
+    "description":"Notifiy service"
+    },
     {"port": "8080",
-     "description":"Database service"
-     },
+    "description":"Database service"
+    },
     {"port": "8041",
-     "description":"Messaging service"
-     },
+    "description":"Messaging service"
+    },
     {"port": "8082",
-     "description":"Triage service"
-     },
+    "description":"Triage service"
+    },
     {"port": "8083",
-     "description":"Reports service"
-     },
+    "description":"Reports service"
+    },
     {"port": "8084",
-     "description":"Lab service"
-     },
+    "description":"Lab service"
+    },
 ]
 
-timeInterval = 0.05
-
-port = [sub['port']for sub in services_open]
-description = [sub['description']for sub in services_open]
-
-ports_closed = [sub['port']for sub in services_open]
-description_of_ports = [sub['description']for sub in services_open]
-
-# This function sends emails to the system admin: it accepts the message content as a parameter
-def mail(message):
-    mail_content = message
-    #The mail addresses and password
-    sender_address = 'blinxcorporation@gmail.com'
-    sender_pass = 'Katumo@4211'
-    receiver_address = adminEmail
-    #Setup the MIME
-    message = MIMEMultipart()
-    message['From'] = sender_address
-    message['To'] = receiver_address
-    message['Subject'] = 'Alert! Service Notification'   #The subject line
-    #The body and the attachments for the mail
-    message.attach(MIMEText(mail_content, 'plain'))
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-    session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
-    text = message.as_string()
-    session.sendmail(sender_address, receiver_address, text)
-    session.quit()
-    print('Mail Sent')
+def mss(adminEmail, hostname, timeInterval, services_open, services_closed):
     
-  
-def func():
-    response = os.system("ping " + hostname)
-        # and then check the response...
-    if response == 0:
-    
-        pingstatus = "Network Active"
-        print(pingstatus)
-            
-    else:
-        pingstatus = "Network Error"
-        print(pingstatus)
-        mail( hostname + ' server is down')
+    port = [sub['port']for sub in services_open]
+    description = [sub['description']for sub in services_open]
 
-     #socket function for open ports
-    for element in port:
-        new_element = int(element)
-        ind = port.index(element)
-        des = description[ind]
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((hostname, new_element))
-        if result == 0:
-            print(des + ' port is working')
-            
+    ports_closed = [sub['port']for sub in services_closed]
+    description_of_ports = [sub['description']for sub in services_closed]
+
+    # This function sends emails to the system admin: it accepts the message content as a parameter
+    def mail(message):
+        mail_content = message
+        #The mail addresses and password
+        sender_address = 'blinxcorporation@gmail.com'
+        sender_pass = 'Katumo@4211'
+        receiver_address = adminEmail
+        #Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        message['Subject'] = 'Alert! Service Notification'   #The subject line
+        #The body and the attachments for the mail
+        message.attach(MIMEText(mail_content, 'plain'))
+        #Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        session.starttls() #enable security
+        session.login(sender_address, sender_pass) #login with mail_id and password
+        text = message.as_string()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+        print('Mail Sent')
+        
+    # This function pings the IP resource and inspects the service ports given  
+    def func():
+        response = os.system("ping -c 1" + hostname)
+            # and then check the response...
+        if response == 0:
+            pingstatus = "Network Active"
+            print(pingstatus)      
         else:
-            mail(des + ' not working' + ' in ' + hostname )
-            print(des + ' not working' + ' in ' + hostname)
-     #socket function for closed ports
-    for port_element in ports_closed:
-        new_port_element = int(port_element)
-        indx = port.index(port_element)
-        desc = description_of_ports[indx]
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((hostname, new_port_element))
-        if result == 0:
-            mail(desc + ' working' + ' in ' + hostname + " on port "+ port_element +" (Should be closed)" )
-            print(desc + ' port is working')
-            
-        else:
-            
-            print(desc + ' not working' + ' in ' + hostname)
-  
-#scheduling the time interval in which the function will execute
-schedule.every(timeInterval).minutes.do(func)
-  
-while True:
-    schedule.run_pending()
-    time.sleep(timeInterval)
+            pingstatus = "Network Error"
+            print(pingstatus)
+            mail( hostname + ' server is down')
+
+        #socket function for open ports
+        for element in port:
+            new_element = int(element)
+            ind = port.index(element)
+            des = description[ind]
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((hostname, new_element))
+            if result == 0:
+                print(des + ' port is working')
+                
+            else:
+                mail(des + ' not working' + ' in ' + hostname )
+                print(des + ' not working' + ' in ' + hostname)
+
+        #socket function for closed ports
+        for port_element in ports_closed:
+            new_port_element = int(port_element)
+            indx = ports_closed.index(port_element)
+            desc = description_of_ports[indx]
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((hostname, new_port_element))
+            if result == 0:
+                mail(desc + ' working' + ' in ' + hostname + " on port "+ port_element +" (Should be closed)" )
+                print(desc + ' port is working')
+                
+            else:
+                print(desc + ' not working' + ' in ' + hostname)
+
+    #scheduling the time interval in which the function will execute
+    schedule.every(timeInterval).minutes.do(func)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(timeInterval)
+
+# Calling the method with the required parameters
+mss(adminEmail, hostname, timeInterval, services_open, services_closed)
